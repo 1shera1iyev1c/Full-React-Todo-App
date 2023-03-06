be205abb292288e0818760dd8c23f2be66c6e33b
1shera1iyev1c <1developer1cccc@gmail.com>
2023-03-06 18:40:34 +0300

    Add time

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ export default function App() {
 
   const [todo, setTodo] = useState([])
   const count = Math.round(Math.random() * 10)
+  const date = new Date();
 
   function handleAddTodo(evt) {
     
     if (evt.target.value) {
        if (evt.keyCode === 13) {
           const newTodo = {
-          id: count,
-          text : evt.target.value,
-          isComplate : false,
+            id: count,
+            timeHour : date.getHours(),
+            timeMin : date.getMinutes(),
+            timeSec : date.getSeconds(),
+            text : evt.target.value,
+            isComplate : false,
         }
         setTodo([newTodo, ...todo])
         evt.target.value = null
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,54 +1,71 @@
-import React, { useRef } from 'react'
+import React, { useRef } from "react";
 
-export default function Item({todos, allTodo, setTodo}) {
-
-  const elTitle = useRef(null)
+export default function Item({ todos, allTodo, setTodo }) {
+  const elTitle = useRef(null);
 
   function handleDelTodo(delTodo) {
-    const filtered = allTodo.filter(todo => todo.id != delTodo)
+    const filtered = allTodo.filter((todo) => todo.id != delTodo);
 
-    setTodo(filtered)
+    setTodo(filtered);
   }
 
   function handleEditTodo(todo) {
-    const editTodo = prompt('Edit text', todos.text)
+    const editTodo = prompt("Edit text", todos.text);
 
-    allTodo.forEach((element) =>{
+    allTodo.forEach((element) => {
       if (element.id === todo.id) {
-        element.text = editTodo
-        setTodo([...allTodo])
+        element.text = editTodo;
+        setTodo([...allTodo]);
       }
-    })
+    });
   }
 
-  function handleCheckTodo (evt, todoId){
+  function handleCheckTodo(evt, todoId) {
     console.log(evt.target.checked);
-    allTodo.forEach(element =>{
+    allTodo.forEach((element) => {
       if (element.id === todoId) {
-        element.isComplate = evt.target.checked
-        setTodo([...allTodo])
+        element.isComplate = evt.target.checked;
+        setTodo([...allTodo]);
       }
-    })
+    });
 
     if (evt.target.checked) {
-      elTitle.current.style.textDecoration = 'line-through';
+      elTitle.current.style.textDecoration = "line-through";
     } else {
-      elTitle.current.style.textDecoration = 'none';
+      elTitle.current.style.textDecoration = "none";
     }
   }
 
   return (
-    
-    <li className='w-100 pe-4 ps-4 pt-2 pb-2 d-flex justify-content-between align-items-center mt-3 border rounded'>
-        <div className='d-flex align-items-center '>
-        <input onChange={(evt) => handleCheckTodo(evt, todos.id)} type="checkbox" />
-        <h3 className='ms-5' ref={elTitle}>{todos.text}</h3>
-        </div>
-        <div>
-        <button className='btn btn-primary me-3' onClick={() => handleEditTodo(todos)}>Edit</button>
-        <button className='btn btn-danger' onClick={() => handleDelTodo(todos.id)}>Del</button>
-        </div>
+    <li className="w-100 pe-4 ps-4 pt-2 pb-2 d-flex justify-content-between align-items-center mt-3 border rounded">
+      <div className="d-flex align-items-center ">
+        <input
+          onChange={(evt) => handleCheckTodo(evt, todos.id)}
+          type="checkbox"
+        />
+        <h3 className="ms-5" ref={elTitle}>
+          {todos.text}
+        </h3>
+      </div>
+      <div className="d-flex rounded btn btn-secondary">
+        <span className="text-light">{todos.timeHour}:</span>
+        <span className="text-light">{todos.timeMin}:</span>
+        <span className="text-light">{todos.timeSec}</span>
+      </div>
+      <div>
+        <button
+          className="btn btn-primary me-3"
+          onClick={() => handleEditTodo(todos)}
+        >
+          Edit
+        </button>
+        <button
+          className="btn btn-danger"
+          onClick={() => handleDelTodo(todos.id)}
+        >
+          Del
+        </button>
+      </div>
     </li>
-    
-  )
+  );
 }
